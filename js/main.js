4dcfca66785652201ea9105f7efc3420a08188fb
Andrew Locke <alocke12992@gmail.com>
2018-02-14 23:38:49 -0700

    rebase

--- js/main.js
+++ js/main.js
@@ -6,38 +6,36 @@ $(document).ready( function(){
   var lose = 0
   var ties = 0
   var gamesPlayed = 0
-  var user 
-  var computer 
+  var user
+  var computer
+  var clear 
 
-  $('#start').on('click', function () {
-    startButton = $(this)
-    showGame = true
-    toggle(true)
+  $('.tooltipped').tooltip({ delay: 50 })
+  
+
+  $('#start').click(function () {
+    $('#start').removeClass('scale-in').addClass('scale-out')
+    $('#reset').removeClass('scale-out').addClass('scale-in')
+    $('#game').removeClass('scale-out').addClass('scale-in')
   })
 
-  $('#reset').on('click', function () {
-    //Need to figure out how to clear the items here 
-    toggle(false)
-    
-  }) 
+  $('#reset').click(function () {
+    $('#reset').removeClass('scale-in').addClass('scale-out')
+    $('#start').removeClass('scale-out').addClass('scale-in')
+    $('#game').removeClass('scale-in').addClass('scale-out')
+    clear() 
+  })
 
-  function toggle(bool){
-    var showGame = bool 
-    var game = $('#game')
-    if (showGame === true) {
-        game.show()
-        startButton.hide() 
-
-      } else {
-        game.hide()
-        startButton.show()
-      // $('#win').text(0)
-      // $('#lose').text(0)
-      // $('#tie').text(0)
-      // $('#games_played').text(0)
-  
-      } 
-    }
+  function clear() {
+    win = 0
+    $('#win').text(0)
+    lose = 0
+    $('#lose').text(0)
+    ties = 0
+    $('#tie').text(ties)
+    gamesPlayed = 0
+    $('#games_played').text(gamesPlayed)
+  }
 
   function updateScore(result) {
     switch (result) {
@@ -52,23 +50,24 @@ $(document).ready( function(){
       default:
         ties++
         $('#tie').text(ties)
+      
     }
     gamesPlayed++
     $('#games_played').text(gamesPlayed)
-    
   }
 
-  function displayResult(result, comChoice) {
+  function displayResult(result, compChoice) {
+    debugger
     if (result == 'Win') {
-      $('#userChoice').text("You chose" + user + ".")
+      $('#userChoice').text("You chose " + user + ".")
       $('#compChoice').text("The computer chose " + computer + ".")
       $('#result').text("You Win")
     } else if (result == 'Loss') {
-      $('#userChoice').text("You chose" + user + ".")
+      $('#userChoice').text("You chose " + user + ".")
       $('#compChoice').text("The computer chose " + computer + ".")
       $('#result').text("You lose.")
     } else {
-      $('#userChoice').text("You chose" + user + ".")
+      $('#userChoice').text("You chose " + user + ".")
       $('#compChoice').text("The computer chose " + computer + ".")
       $('#result').text("You tied.")
     }
@@ -76,35 +75,36 @@ $(document).ready( function(){
 
   $('.choice').on('click', function() {
     user = this.id
+    Materialize.toast(('You chose ' + user + '.'), 4000)
     computer = rps[Math.floor(Math.random() * rps.length)]
-    
-    if (user === computer) {
-      updateScore('Tie')
+  
+    if (user == computer) {
+      updateScore('Tie', computer)
     }
     switch (user) {
       case 'Rock':
-        if (computer === 'Scissors') {
+        if (computer == 'Scissors') {
           displayResult('Win', computer)
           updateScore('Win')
-        } else if (computer === 'Paper') {
+        } else if (computer == 'Paper') {
           displayResult('Lose', computer)
           updateScore('Lose')
         }
         break
       case 'Scissors':
-        if (computer === 'Paper') {
+        if (computer == 'Paper') {
           displayResult('Win', computer)
           updateScore('Win')
-        } else if (computer === 'Rock') {
+        } else if (computer == 'Rock') {
           displayResult('Lose', computer)
           updateScore('Lose')
         }
         break
       case 'Paper':
-        if (computer === 'Rock') {
+        if (computer == 'Rock') {
           displayResult('Win', computer)
           updateScore('Win')
-        } else if (computer === 'Scissors') {
+        } else if (computer == 'Scissors') {
           displayResult('Lose', computer)
           updateScore('Lose')
         }
